1348f2d0ad95335b88714ccfb22faf76d827170a
jp-bitcube <jp@bitcube.tech>
2024-08-29 11:44:16 +0200

    feat: add validation

--- src/users/dto/create-users.dto.ts
+++ src/users/dto/create-users.dto.ts
@@ -1,7 +1,16 @@
+import { IsEmail, IsEnum, IsString, IsNotEmpty } from 'class-validator';
 import { Role } from '../users.type';
 
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsEnum(['INTERN', 'ADMIN', 'ENGINEER'], {
+    message: 'Valid role required',
+  })
   role: Role;
 }
